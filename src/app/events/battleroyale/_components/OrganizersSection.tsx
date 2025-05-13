2abda89db75408cf39826f5c37f05f20244cdfe7
Ville Strengell <ville.strengell@aalto.fi>
2025-05-13 09:28:46 +0300

    apostrophes

--- src/app/events/battleroyale/_components/OrganizersSection.tsx
+++ src/app/events/battleroyale/_components/OrganizersSection.tsx
@@ -22,9 +22,9 @@ export const OrganizersSection = () => {
             </div>
             <h3 className="mt-6 text-2xl font-bold">UNI LIFE</h3>
             <p className="mt-4 max-w-md text-lg text-balance text-muted-foreground">
-              Professionally organized student events that are unlike anything you've seen before.
+              Professionally organized student events that are unlike anything you&apos;ve seen before.
               <br /><br />
-              We guarantee you'll have fun - every time!
+              We guarantee you&apos;ll have fun - every time!
             </p>
           </div>
           
